b6fe290ad5b6b4c74666c71eef3c963a7ec7e72a
chenk <chenk@apical.com.cn>
2020-09-11 15:30:55 +0800

    implement dead link check.

--- ffrdp/ffrdp.h
+++ ffrdp/ffrdp.h
@@ -5,6 +5,7 @@ void* ffrdp_init  (char *ip, int port, int server);
 void  ffrdp_free  (void *ctxt);
 int   ffrdp_send  (void *ctxt, char *buf, int len);
 int   ffrdp_recv  (void *ctxt, char *buf, int len);
+int   ffrdp_isdead(void *ctxt);
 void  ffrdp_update(void *ctxt);
 void  ffrdp_dump  (void *ctxt);
 
